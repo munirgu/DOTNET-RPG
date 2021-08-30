048340459dda2ead2d138fa1d32e4a25751636fc
munirgu <munirgu@maestralsolutions.com>
2021-08-30 09:53:19 +0200

    Character class

---
+++ Models/Charcter.cs
@@ -0,0 +1,12 @@
+namespace DOTNET_RPG.Models
+{
+    public class Charcter
+    {
+        public int Id {get; set;}
+        public String Name {get; set;} = "Frodo";
+        public int HitPoints {get; set;} =100;
+        public int Strength {get; set;} = 10;
+        public int Defense {get; set;} = 10;
+        public int Intelligence {get; set;} = 10;
+    }
+}

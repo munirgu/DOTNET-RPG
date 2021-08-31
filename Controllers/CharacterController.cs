4251c9510179b181e203fc96e2369c03b7ef9861
munirgu <munirgu@maestralsolutions.com>
2021-08-31 10:11:28 +0200

    Adding Controller

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+using DOTNET_RPG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOTNET_RPG.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+        
+        [HttpGet]
+        public ActionResult <Character> Get(){
+            return Ok(knight);
+        }
+    }
+}
--- Models/Character.cs
+++ Models/Character.cs
@@ -1,13 +1,16 @@
 namespace DOTNET_RPG.Models
 {
-    public class Charcter
+    public class Character
     {
-        public int Id {get; set;}
-        public String Name {get; set;} = "Frodo";
-        public int HitPoints {get; set;} =100;
-        public int Strength {get; set;} = 10;
-        public int Defense {get; set;} = 10;
-        public int Intelligence {get; set;} = 10;
-        public RpgClass class {get; set;} = RpgClass.Knight;
+        public int Id { get; set; }
+        public string Name { get; set; } ="Frodo";
+        public int HitPoints { get; set; }=100;
+        public int Strength { get; set; } =10;
+        public int Defense { get; set; } =10;
+        public int Intelligence { get; set; } =10;
+
+    public RpgClass Class {get; set;}= RpgClass.Knight;
+
+
     }
 }
